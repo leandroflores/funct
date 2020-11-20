0b900de39135527ca31909f2eb1a6ce334e18559
leandroflores <leandroflores7@gmail.com>
2020-11-20 19:32:07 -0200

    - Adicionada a classe FunctArray.

---
+++ Funct/src/funct/FunctArray.java
@@ -0,0 +1,170 @@
+package funct;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * <p>Classe de Biblioteca <b>FunctArray</b>.</p>
+ * <p>Classe responsavel pelas operacoes envolvendo <b>Array</b> e <b>List</b>.</p>
+ * @author Leandro
+ * @since  20/11/2020
+ * @see    java.util.Arrays
+ * @see    java.util.List
+ */
+public class FunctArray {
+    
+    /**
+     * Metodo responsavel por retornar o Maior Elemento de um Array.
+     * @param  array Array.
+     * @return Maior Elemento de um Array.
+     */
+    public Integer max(Integer[] array) { 
+        if (array.length == 0)
+            return null;
+        Integer max = Integer.MIN_VALUE;
+        for (Integer current : array)
+                max = current > max ? current : max;
+        return  max;
+    }
+    
+    /**
+     * Metodo responsavel por retornar o Menor Elemento de um Array.
+     * @param  array Array.
+     * @return Menor Elemento de um Array.
+     */
+    public Integer min(Integer[] array) { 
+        if (array.length == 0)
+            return null;
+        Integer min = Integer.MAX_VALUE;
+        for (Integer current : array)
+                min = current < min ? current : min;
+        return  min;
+    }
+    
+    /**
+     * Metodo responsavel por retornar a Soma dos Elementos de um Array.
+     * @param  array Array.
+     * @return Soma dos Elementos de um Array.
+     */
+    public Integer sum(Integer[] array) { 
+        if (array.length == 0)
+            return null;
+        Integer sum  = 0;
+        for (Integer current : array)
+                sum += current;
+        return  sum;
+    }
+    
+    /**
+     * Metodo responsavel por retornar a Media Simples dos Elementos de um Array.
+     * @param  array Array.
+     * @return Media Simples dos Elementos de um Array.
+     */
+    public Integer avg(Integer[] array) { 
+        if (array.length == 0)
+            return null;
+        return sum(array) / array.length;
+    }
+    
+    /**
+     * Metodo responsavel por incrementar um Valor nos Elementos de um Array.
+     * @param  array Array.
+     * @param  value Valor para Incrementar.
+     */
+    public void increment(Integer[] array, Integer value) { 
+        for (int i = 0; i < array.length; i++)
+            array[i] += value;
+    }
+    
+    /**
+     * Metodo responsavel por decrementar um Valor nos Elementos de um Array.
+     * @param  array Array.
+     * @param  value Valor para Decrementar.
+     */
+    public void decrement(Integer[] array, Integer value) { 
+        for (int i = 0; i < array.length; i++)
+            array[i] -= value;
+    }
+    
+    /**
+     * Metodo responsavel por retornar a Frequencia de um Elemento no Array.
+     * @param  array Array.
+     * @param  element Elemento.
+     * @return Frequencia de um Elemento no Array.
+     */
+    public Integer frequency(Integer[] array, Integer element) {
+        Integer frequency = 0;
+        for (Integer current : array) {
+            if (current.equals(element))
+                frequency++; 
+        }
+        return  frequency;
+    }
+    
+    /**
+     * Metodo responsavel por retornar o Indice de um Elemento no Array.
+     * @param  array Array.
+     * @param  element Elemento.
+     * @return Indice de um Elemento no Array.
+     */
+    public Integer indexOf(Integer[] array, Integer element) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals(element))
+                return i; 
+        }
+        return -1;
+    }
+    
+    /**
+     * Metodo responsavel por retornar o Ultimo Indice de um Elemento no Array.
+     * @param  array Array.
+     * @param  element Elemento.
+     * @return Ultimo Indice de um Elemento no Array.
+     */
+    public Integer lastIndexOf(Integer[] array, Integer element) {
+        for (int i = array.length - 1; i >= 0; i--) {
+            if (array[i].equals(element))
+                return i; 
+        }
+        return -1;
+    }
+    
+    /**
+     * Metodo responsavel por adicionar um Elemento no Array.
+     * @param array Array.
+     * @param element Novo Elemento.
+     */
+    public void add(Integer[] array, Integer element) {
+        array = Arrays.copyOf(array, array.length + 1);
+        array[array.length - 1] = element;
+    }
+    
+    /**
+     * Metodo responsavel por ordenar crescentemente os Elementos de um Array.
+     * @param array Array.
+     */
+    public void sort(Integer[] array) {
+        Arrays.sort(array);
+    }
+    
+    /**
+     * Metodo responsavel por ordenar decrescentemente os Elementos de um Array.
+     * @param array Array.
+     */
+    public void reverse(Integer[] array) {
+        Arrays.sort(array, Collections.reverseOrder());
+    }
+    
+    /**
+     * Metodo responsavel por retornar a Array em uma String.
+     * @param  array Array.
+     * @return Array em uma String.
+     */
+    public String getString(Integer[] array) {
+        String  string  = "[";
+        Integer lastPos = array.length - 1;
+        for (int i = 0; i < lastPos; i++)
+                string += array[i] + ", ";
+        return  string +  array[lastPos] + "]";
+    }
+}
---
+++ Funct/test/funct/FunctStringTest.java
@@ -0,0 +1,176 @@
+package funct;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * <p>Classe de Testes <b>FunctStringTest</b>.</p>
+ * <p>Classe responsavel por Testar os metodos da Classe <b>FunctString</b>.</p>
+ * @author Leandro
+ * @since  20/11/2020
+ * @see    funct.FunctString
+ */
+public class FunctStringTest {
+    private final FunctString funct;
+    
+    /**
+     * Metodo construtor padrao da Classe.
+     */
+    public FunctStringTest() {
+        funct = new FunctString();
+    }
+
+    /**
+     * Metodo responsavel por testar o metodo getString(Character character, Integer size).
+     */
+    @Test
+    public void testGetString() {
+        assertEquals(funct.getString(' ', -5),  "");
+        assertEquals(funct.getString('a', 0),   "");
+        assertEquals(funct.getString('C', 1),   "C");
+        assertEquals(funct.getString('|', 5),   "|||||");
+        assertEquals(funct.getString(' ', 6),   "      ");
+        assertEquals(funct.getString('z', 10),  "zzzzzzzzzz");
+    }
+    
+    /**
+     * Metodo responsavel por testar o metodo getEspacos(int size).
+     */
+    @Test
+    public void testGetEspacos() {
+        assertEquals(funct.getEspacos(-5),  "");
+        assertEquals(funct.getEspacos(-1),  "");
+        assertEquals(funct.getEspacos(0),   "");
+        assertEquals(funct.getEspacos(1),   " ");
+        assertEquals(funct.getEspacos(5),   "     ");
+        assertEquals(funct.getEspacos(10),  "          ");
+        assertEquals(funct.getEspacos(20),  "                    ");
+    }
+    
+    /**
+     * Metodo responsavel por testar o metodo toRight(String string, int size).
+     */
+    @Test
+    public void testToRigth() {
+        assertEquals(funct.toRight("String", -1), "String");
+        assertEquals(funct.toRight("String", 1),  "String");
+        assertEquals(funct.toRight("String", 6),  "String");
+        assertEquals(funct.toRight("String", 7),  " String");
+        assertEquals(funct.toRight("String", 10), "    String");
+    }
+    
+    /**
+     * Metodo responsavel por testar o metodo toLeft(String string, int size).
+     */
+    @Test
+    public void testToLeft() {
+        assertEquals(funct.toLeft("String", -1), "String");
+        assertEquals(funct.toLeft("String", 1),  "String");
+        assertEquals(funct.toLeft("String", 6),  "String");
+        assertEquals(funct.toLeft("String", 7),  "String ");
+        assertEquals(funct.toLeft("String", 10), "String    ");
+    }
+    
+    /**
+     * Metodo responsavel por testar o metodo toCenter(String string, int size).
+     */
+    @Test
+    public void testToCenter() {
+        assertEquals(funct.toCenter("String", -1), "String");
+        assertEquals(funct.toCenter("String", 1),  "String");
+        assertEquals(funct.toCenter("String", 6),  "String");
+        assertEquals(funct.toCenter("String", 7),  "String ");
+        assertEquals(funct.toCenter("String", 10), "  String  ");
+        assertEquals(funct.toCenter("Strin",  10), "  Strin   ");
+    }
+    
+    /**
+     * Metodo responsavel por testar o metodo countChar(String string, char character).
+     */
+    @Test
+    public void testCountChar() {
+        assertEquals(funct.countChar("", ' '), new Long(0));
+        assertEquals(funct.countChar("B", 'B'),  new Long(1));
+        assertEquals(funct.countChar("  ", ' '), new Long(2));
+        assertEquals(funct.countChar(" A A ", 'a'), new Long(0));
+        assertEquals(funct.countChar("BBBBBC", 'B'),  new Long(5));
+        assertEquals(funct.countChar("ddddddd", 'd'), new Long(7));
+    }
+    
+    /**
+     * Metodo responsavel por testar o metodo reverse(String string).
+     */
+    @Test
+    public void testReverse() {
+        assertEquals(funct.reverse(""),  "");
+        assertEquals(funct.reverse("0"), "0");
+        assertEquals(funct.reverse("AbbA"), "AbbA");
+        assertEquals(funct.reverse("CCAA"), "AACC");
+        assertEquals(funct.reverse("reverse"), "esrever");
+        assertEquals(funct.reverse("00aaa00"), "00aaa00");
+    }
+    
+    /**
+     * Metodo responsavel por testar o metodo initUpperCase(String string).
+     */
+    @Test
+    public void testInitUpperCase() {
+        assertEquals(funct.initUpperCase(""),   "");
+        assertEquals(funct.initUpperCase("a"),  "A");
+        assertEquals(funct.initUpperCase("aA"), "Aa");
+        assertEquals(funct.initUpperCase("STRING"),   "String");
+        assertEquals(funct.initUpperCase("Str_Test"), "Str_test");
+        assertEquals(funct.initUpperCase("Aaabbbcc"), "Aaabbbcc");
+    }
+    
+    /**
+     * Metodo responsavel por testar o metodo getInitUpperCase(String string).
+     */
+    @Test
+    public void testGetInitUpperCase() {
+        assertEquals(funct.getInitUpperCase(""),   "");
+        assertEquals(funct.getInitUpperCase("a"),  "A");
+        assertEquals(funct.getInitUpperCase("a bc"),   "A Bc");
+        assertEquals(funct.getInitUpperCase(" a bc "), "A Bc");
+        assertEquals(funct.getInitUpperCase(" a_BC "),  "A_bc");
+        assertEquals(funct.getInitUpperCase("a b c d"), "A B C D");
+    }
+    
+    /**
+     * Metodo responsavel por testar o metodo getNumeros(String string).
+     */
+    @Test
+    public void testGetNumeros() {
+        assertEquals(funct.getNumeros(""),  "");
+        assertEquals(funct.getNumeros("A"), "");
+        assertEquals(funct.getNumeros(" 1.72 "),   "172");
+        assertEquals(funct.getNumeros("R$ 10,20"),  "1020");
+        assertEquals(funct.getNumeros("A3 .,72 0"), "3720");
+        assertEquals(funct.getNumeros("Ajfbafnda"), "");
+    }
+    
+    /**
+     * Metodo responsavel por testar o metodo removeCharEspeciais(String string).
+     */
+    @Test
+    public void testRemoveCharEspeciais() {
+        assertEquals(funct.removeCharEspeciais(""),    "");
+        assertEquals(funct.removeCharEspeciais("Â1"),  "A1");
+        assertEquals(funct.removeCharEspeciais("123"), "123");
+        assertEquals(funct.removeCharEspeciais("normal"),  "normal");
+        assertEquals(funct.removeCharEspeciais("áêíõüã"),  "aeioua");
+        assertEquals(funct.removeCharEspeciais("ÂÇÊÍ01 "), "ACEI01 ");
+    }
+    
+    /**
+     * Metodo responsavel por testar o metodo removeCharEspeciais(String string).
+     */
+    @Test
+    public void testMd5() {
+        assertEquals(funct.md5(""),  "d41d8cd98f00b204e9800998ecf8427e");
+        assertEquals(funct.md5("A"), "7fc56270e7a70fa81a5935b72eacbe29");
+        assertEquals(funct.md5("12345"), "827ccb0eea8a706c4c34a16891f84e7b");
+        assertEquals(funct.md5("SENHA"), "85ee0fe4f155a9af2657d85054ad63ca");
+        assertEquals(funct.md5("senha"), "e8d95a51f3af4a3b134bf6bb680a213a");
+    }
+}
