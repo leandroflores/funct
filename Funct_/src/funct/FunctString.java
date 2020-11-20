ad63e1b254f0fa6da592bdd1085e4df9ca7037dd
leandroflores <leandroflores7@gmail.com>
2020-11-20 17:37:59 -0200

    - Adicionado métodos para a classe FunctString. - Adicionado testes da classe FunctString. - Adicionada a classe FunctDate. - Adicionado os testes da classe FunctDate.

--- Funct_/src/funct/FunctString.java
+++ Funct_/src/funct/FunctString.java
@@ -1,14 +1,29 @@
 package funct;
 
-import java.util.StringTokenizer;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.Normalizer;
+import java.util.regex.Pattern;
 
 /**
  * <p>Classe de Biblioteca <b>FunctString</b>.</p>
  * <p>Classe responsavel pelas operacoes envolvendo <b>String</b>.</p>
  * @author Leandro
  * @since  20/11/2020
+ * @see    java.lang.String
  */
 public class FunctString {
+    
+    /**
+     * Metodo responsavel por retornar uma String com um determinado tamanho contendo o char.
+     * @param  character Caractere do Conteudo.
+     * @param  size Tamanho da String.
+     * @return String composta pelo caracter.
+     */
+    public String getString(Character character, Integer size) {
+        return size > 0 ? new String(new char[size]).replaceAll("\0", Character.toString(character)) : "";
+    }
     
     /**
      * Metodo responsavel por retornar uma String contendo espacos.
@@ -16,7 +31,7 @@ public class FunctString {
      * @return String contendo espaços.
      */
     public String getEspacos(Integer size) {
-        return size > 0 ? new String(new char[size]).replaceAll("\0", " ") : "";
+        return this.getString(' ', size);
     }
     
     /**
@@ -64,11 +79,75 @@ public class FunctString {
      * @param  character Caracter a ser contado.
      * @return Quantidade de Char na String.
      */
-    public Long countChar(String string, char character) {
+    public Long countChar(String string, Character character) {
         return string.codePoints().filter(current -> current == character).count();
     }
     
-    public static void main(String[] args) {
-        System.out.println(new FunctString().countChar("  A  A ", 'A'));
+    /**
+     * Metodo responsavel por retornar a String em ordem inversa.
+     * @param  string Cadeia de caracteres inicial.
+     * @return String em ordem inversa.
+     */
+    public String reverse(String string) {
+        return new StringBuilder(string).reverse().toString();
+    }
+    
+    /**
+     * Metodo responsavel por retornar uma String com o primeiro char maiusculo.
+     * @param  string String inicial.
+     * @return String com o primeiro char maiusculo.
+     */
+    public String initUpperCase(String string) {
+        if (string.length() == 0) 
+            return "";
+        return   string.substring(0, 1).toUpperCase() 
+               + string.substring(1).toLowerCase();
+    }
+    
+    /**
+     * Metodo responsavel por retornar as iniciais maiusculas de uma String.
+     * @param  string String inicial.
+     * @return String com as iniciais maiusculas.
+     */
+    public String getInitUpperCase(String string) {
+        String upper  = "";
+        for (String current : string.split(" "))
+               upper += initUpperCase(current) + " ";
+        return upper.trim();
+    }
+    
+    /**
+     * Metodo responsavel por retornar uma String apenas com os numeros da String passada como parametro.
+     * @param  string String inicial.
+     * @return String apenas com os numeros.
+     */
+    public String getNumeros(String string) {
+        return string.replaceAll("\\D+", "");
+    }
+    
+    /**
+     * Metodo responsavel por remover os caracteres especiais de uma String.
+     * @param  string String inicial.
+     * @return String sem caracteres especiais.
+     */
+    public String removeCharEspeciais(String string) {
+        String  normalized = Normalizer.normalize(string, Normalizer.Form.NFD);
+        Pattern pattern    = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
+        return  pattern.matcher(normalized).replaceAll("");
+    }
+    
+    /**
+     * Metodo responsavel por retornar uma String criptografada pelo algoritmo MD5.
+     * @param  string String inicial.
+     * @return String criptografada pelo algoritmo MD5.
+     */
+    public String md5(String string) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("MD5");
+                          digest.update(string.getBytes(), 0, string.length());
+            return new BigInteger(1, digest.digest()).toString(16);
+        }catch (NoSuchAlgorithmException exception) {
+            return null;
+        }
     }
 }
